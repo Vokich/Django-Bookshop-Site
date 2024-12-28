f2ddec70608c55f8f4ca53bbcfc710c24a3a0dcc
Vokich <kolupaevvova638@gmail.com>
2024-12-28 17:20:53 +0300

    Add files via upload

---
+++ bookshop/catalog/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Author, Book, BookInstance, Genre
+from django.views import generic
+from django.views.generic import DetailView
+from .forms import AuthorsForm
+from django.http import *
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Book
+
+
+def index(request):
+    #return HttpResponse("Главная страница БукШопа!")
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+    num_authors = Author.objects.count()
+
+    return render(request, 'index.html',
+                  context={'num_books': num_books,
+                           'num_instances': num_instances,
+                           'num_instances_available': num_instances_available,
+                           'num_authors': num_authors,},
+                  )
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+def authors_add(request):
+    author = Author.objects.all()
+    authorsform = AuthorsForm()
+    return render(request, "catalog/authors_add.html", {"form": authorsform, "author": author})
+
+def create(request):
+    if request.method == "POST":
+        author = Author()
+        author.first_name = request.POST.get("first_name")
+        author.last_name = request.POST.get("last_name")
+        author.date_of_birth = request.POST.get("date_of_birth")
+        author.date_of_death = request.POST.get("date_of_death")
+        author.save()
+        return HttpResponseRedirect("/authors_add/")
+
+def delete(request, id):
+    try:
+        author = Author.objects.get(id=id)
+        author.delete()
+        return HttpResponseRedirect("/authors_add/")
+    except Author.DoesNotExist:
+        return HttpResponseNotFound("<h2>Автор не найден</h2>")
+
+def edit1(request, id):
+    author = Author.objects.get(id=id)
+    if request.method == "POST":
+        author.first_name = request.POST.get("first_name")
+        author.last_name = request.POST.get("last_name")
+        author.date_of_birth = request.POST.get("date_of_birth")
+        author.date_of_death = request.POST.get("date_of_death")
+        author.save()
+        return HttpResponseRedirect("/authors_add/")
+    else:
+        return render(request, "edit1.html", {"author": author})
+
+class BookCreate(CreateView):
+    model = Book
+    fields = '__all__'
+    success_url = reverse_lazy('books')
+
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = '__all__'
+    success_url = reverse_lazy('books')
+
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = reverse_lazy('books')
+
